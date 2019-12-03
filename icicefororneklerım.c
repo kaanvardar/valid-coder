08ecb3e602f369c9b5d4a1f8e846738d830b231f
Kaan Vardar <kaanvardar@github.com>
2019-12-04 02:47:47 +0300

    Project Added
    For Examples for C

---
+++ icicefororneklerım.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int main()
+{
+	int i=0,j=0;
+	
+	for (i=0; i<=3; i++)
+	{
+		for(j=0; j<=5; j++)
+		{
+			printf("hi world\n");
+		}
+		
+		printf("www.sample.com\n");	
+	}
+}
+
+
+
+int main()
+{
+	int i=1,j=1;
+	
+	for(i=1; i<=10; i++)
+	{
+		for(j=1; j<=10; j++)
+		{
+			printf("%d x %d = %d\n",i,j,(i*j));
+		} //bu süslü parantezin hemen üstüne yani içerdeki for döngüsüne geri döner.
+		  // 11 oldugunda j<=10 'u saðlamadýðý için dýþardaki for döngüsüne geçer.
+		printf("\n");	
+	}
+}
+
+
+
+
+int main()
+{
+	int i=0,j=0;
+	
+	for(i=1; i<=10; i++)
+	{
+		for(j=1; j<=20; j++)
+		{
+			printf("*");	
+		}
+		printf("\n");
+	}
+}
+
+*/
+
+int main()
+{
+	int i,j,k;
+	
+	for(i=0; i<=0; i++)
+	{
+		for(j=0; j<=5; j++)
+		{
+			printf("*\t*\n");
+		}
+		
+		for(k=0; k<=0; k++)
+		{
+			printf("*********\n");
+		}
+		
+		for(j=0; j<=5; j++)
+		{
+			printf("*\t*\n");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
